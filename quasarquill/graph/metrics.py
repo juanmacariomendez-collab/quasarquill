2f2563c3f4e2b47a69032e6aeff31163140c86b8
juanmacariomendez-collab <juanmacariomendez@gmail.com>
2025-12-02 15:40:12 +0000

    Tidy up minor inconsistencies

--- quasarquill/graph/metrics.py
+++ quasarquill/graph/metrics.py
@@ -8,3 +8,5 @@ def in_degree(g: Dict[str, List[str]]):
     return inc
 def top_by_out(g: Dict[str, List[str]], n:int=5) -> List[Tuple[str,int]]:
     return sorted(((k,len(v)) for k,v in g.items()), key=lambda x:-x[1])[:n]
+
+# autosave 2025-12-02T15:40:11.684161+00:00
