f32fc6e7478acd36f7b63997fe1a216ff3c55700
juanmacariomendez-collab <juanmacariomendez@gmail.com>
2025-12-05 13:27:29 +0000

    Small improvements here and there

--- quasarquill/utils/markdown.py
+++ quasarquill/utils/markdown.py
@@ -4,3 +4,4 @@ def headings(md: str): return [(m.group(1), m.group(2).strip()) for m in HDR.fin
 
 # autosave 2025-10-06T09:36:17.436298+00:00
 # tweak 2025-11-21T11:47:57.437152+00:00
+# tweak 2025-12-05T13:27:28.559777+00:00
