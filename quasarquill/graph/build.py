f3ca6a16d9aa5ad54a629e8dbdab3d9474c26502
juanmacariomendez-collab <juanmacariomendez@gmail.com>
2026-02-24 18:40:43 +0000

    Tidy up mnior inconsistencies

--- quasarquill/graph/build.py
+++ quasarquill/graph/build.py
@@ -34,3 +34,5 @@ def build_graph() -> Dict[str, List[str]]:
 # autosave 2025-12-19T17:23:03.636115+00:00
 
 # autosave 2026-01-09T12:15:14.121854+00:00
+
+# autosave 2026-02-24T18:40:42.799583+00:00
